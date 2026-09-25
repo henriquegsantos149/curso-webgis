f068da295604e6d0f2c06e95b286afdd90f4440c
olivio-pro <olivio@ambientalpro.com.br>
2026-09-25 15:58:53 -0300

    feat: criar copia lp3 a partir da lp2

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        lp2: resolve(__dirname, 'lp2.html')
+        lp2: resolve(__dirname, 'lp2.html'),
+        lp3: resolve(__dirname, 'lp3.html')
       }
     }
   }
